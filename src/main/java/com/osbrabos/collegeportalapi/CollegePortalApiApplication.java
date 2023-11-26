65377f0ca2b0a3ffa3b8ceba8eebda5053ea2e03
ibnergabriel <ibner68@gmail.com>
2023-11-25 21:39:33 -0300

    Primeiro Commit

---
+++ src/main/java/com/osbrabos/collegeportalapi/CollegePortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.osbrabos.collegeportalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegePortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegePortalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osbrabos/collegeportalapi/CollegePortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osbrabos.collegeportalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegePortalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
