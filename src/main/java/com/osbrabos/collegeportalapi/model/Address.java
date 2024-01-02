dde6fffe5678f88697d2a65fb3b38f97ee817dcb
ibnergabriel <ibner68@gmail.com>
2024-01-02 17:46:09 -0300

    refactor: Address as a record

--- src/main/java/com/osbrabos/collegeportalapi/model/Address.java
+++ src/main/java/com/osbrabos/collegeportalapi/model/Address.java
@@ -1,24 +1,13 @@
 package com.osbrabos.collegeportalapi.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
-public class Address {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String country;
-    private String state;
-    private String city;
-    private String street;
-    private String postalCode;
-}
+public record Address(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id, 
+    String country, 
+    String state, 
+    String city, 
+    String street,
+    String postalCode ) {
+} 
